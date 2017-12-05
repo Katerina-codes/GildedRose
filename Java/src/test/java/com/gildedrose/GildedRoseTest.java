b491e98dcd7cbe4f44532754d8afe721e5823a59
Katerina <katerinaldg@gmail.com>
2017-12-05 22:42:48 +0000

    At end of day sellIn and Quality decrease by one

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,12 @@ import org.junit.Test;
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void afterEachDaySellInDateAndQualityDecreaseByOne() {
+        Item[] items = new Item[] { new Item("cake", 1, 1) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(0, app.items[0].quality);
+        assertEquals(0, app.items[0].sellIn);
     }
 
 }
