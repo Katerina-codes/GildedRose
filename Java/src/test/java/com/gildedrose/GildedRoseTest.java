98b749b84ddaea7263bd9ae663a50b94a46775e6
Katerina <katerinaldg@gmail.com>
2017-12-05 22:53:26 +0000

    Quality of Aged Brie increases with age

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -30,4 +30,13 @@ public class GildedRoseTest {
         app.updateQuality();
         assertEquals(0, app.items[0].quality);
     }
+
+    @Test
+    public void qualityOfAgedBrieIncreasesAsItGetsOlder() {
+        Item[] items = new Item[] { new Item("Aged Brie", 2, 1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(2, app.items[0].quality);
+        assertEquals(1, app.items[0].sellIn);
+    }
 }
