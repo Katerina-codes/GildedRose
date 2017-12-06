7a76aad13eaa8877f33fe75128fa3f2a6ba46fcf
Katerina <katerinaldg@gmail.com>
2017-12-06 12:11:15 +0000

    Correct if else statement to isolate Sulfuras
    - Removed references to Sulfuras throughout else statement.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -11,58 +11,53 @@ class GildedRose {
         for (Item item : items) {
             if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
 
-            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality < 50) {
-                    addOneToItemQuality(item);
+            } else {
+                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.quality < 50) {
+                        addOneToItemQuality(item);
 
                         if (item.sellIn < 11) {
                             if (item.quality < 50) {
                                 addOneToItemQuality(item);
                             }
 
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                addOneToItemQuality(item);
+                            if (item.sellIn < 6) {
+                                if (item.quality < 50) {
+                                    addOneToItemQuality(item);
+                                }
                             }
                         }
                     }
-                }
-            } else if (item.name.equals("Aged Brie")) {
-                if (item.quality < 50) {
-                    addOneToItemQuality(item);
-                }
-            } else {
-                if (item.quality > 0) {
-                    subtractOneFromItemQuality(item);
-                }
-            }
-
-            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-
-            } else {
-                item.sellIn = item.sellIn - 1;
-            }
-
-            if (item.sellIn < 0) {
-                if (item.name.equals("Aged Brie")) {
+                } else if (item.name.equals("Aged Brie")) {
                     if (item.quality < 50) {
                         addOneToItemQuality(item);
                     }
                 } else {
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        item.quality = 0;
+                    if (item.quality > 0) {
+                        subtractOneFromItemQuality(item);
+                    }
+                }
+
+                    item.sellIn = item.sellIn - 1;
+                }
+
+                if (item.sellIn < 0) {
+                    if (item.name.equals("Aged Brie")) {
+                        if (item.quality < 50) {
+                            addOneToItemQuality(item);
+                        }
                     } else {
-                        if (item.quality > 0) {
-                            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                continue;
+                        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                            item.quality = 0;
+                        } else {
+                            if (item.quality > 0) {
+                                subtractOneFromItemQuality(item);
                             }
-                            subtractOneFromItemQuality(item);
                         }
                     }
                 }
             }
         }
-    }
 
     private void subtractOneFromItemQuality(Item item) {
         item.quality = item.quality - 1;
