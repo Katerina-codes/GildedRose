ba2bc102933d2e32725f6f09bd9b924aac99e359
Katerina <katerinaldg@gmail.com>
2017-12-06 12:07:23 +0000

    Removed dead code

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -30,19 +30,6 @@ class GildedRose {
             } else if (item.name.equals("Aged Brie")) {
                 if (item.quality < 50) {
                     addOneToItemQuality(item);
-
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                addOneToItemQuality(item);
-                            }
-                        }
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                addOneToItemQuality(item);
-                            }
-                        }
-                    }
                 }
             } else {
                 if (item.quality > 0) {
