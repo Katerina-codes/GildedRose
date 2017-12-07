fc57d21334b553970ca170b6bd5efcf5c4a0f20c
Katerina <katerinaldg@gmail.com>
2017-12-07 09:18:14 +0000

    Corrected indentation

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -38,26 +38,26 @@ class GildedRose {
                     }
                 }
 
-                    item.sellIn = item.sellIn - 1;
-                }
+                item.sellIn = item.sellIn - 1;
+            }
 
-                if (item.sellIn < 0) {
-                    if (item.name.equals("Aged Brie")) {
-                        if (item.quality < 50) {
-                            addOneToItemQuality(item);
-                        }
+            if (item.sellIn < 0) {
+                if (item.name.equals("Aged Brie")) {
+                    if (item.quality < 50) {
+                        addOneToItemQuality(item);
+                    }
+                } else {
+                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        item.quality = 0;
                     } else {
-                        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            item.quality = 0;
-                        } else {
-                            if (item.quality > 0) {
-                                subtractOneFromItemQuality(item);
-                            }
+                        if (item.quality > 0) {
+                            subtractOneFromItemQuality(item);
                         }
                     }
                 }
             }
         }
+    }
 
     private void subtractOneFromItemQuality(Item item) {
         item.quality = item.quality - 1;
